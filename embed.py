79e3b51f7534bea6e649dceb5f867611c0f55dbe
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2022-12-13 23:14:52 +0900

    nyooo

--- embed.py
+++ embed.py
@@ -22,8 +22,16 @@ import itertools
 from lxml import html
 from setuptools import Command
 
+class EmbedSender:
+    async def create_embed (member, channel):
+        # new create
+        _embed = discord.Embed(title="PONG!", description="test 0.0.1",color=0x228b22)
+        _embed.add_field(name = member.name + " join the " + channel.name, value = "Enjoy!")
+        return _embed
 
-async def create_embed (member):
-# new create
-async def edit_embed(member):
-# edit
+    async def edit_embed(member, channel, update):
+    # edit
+        members = [i.name for i in message.author.voice.channel.members]
+        for i in member:
+            update.add_field(name = member.name + " join the " + channel.name, value = "Enjoy!")
+        return update
--- main.py
+++ main.py
@@ -34,12 +34,9 @@ from setuptools import Command
 import envop
 import embed
 
-json_open = open("fortune.json","r")    # opening file
-fortune_list =  json.load(json_open)
 BOT_TOKEN = envop.dot_env_bot_token()   # slashcommand testDotEnv
 GUILD_ID = envop.dot_env_guild_id() # 鯖ID
 CHANNEL_ID = envop.dot_env_channel_id()
-Length = len(fortune_list)  # 名言の個数
 
 ### begin intents
 intents = discord.Intents.default()
@@ -49,26 +46,28 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 ### end intents
 
+"""
 @tree.command(name="add",description="Adding vc channel",guild=discord.Object(id=GUILD_ID))
 async def hlw(interaction) -> None:
+"""
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    # Assign message channel
+    global sendEmbed
+    #  Assign message channel
     if before.channel != after.channel:
         botRoom = client.get_channel(CHANNEL_ID)
         announceChannelIds = [1049940299864080477]
-    if after.channel is not None and after.channel.id in announceChannelIds:
-        if before.channel is None:
-            asyncio.run(embed.create_embed(member))
-        else
-            await embed.edit_embed(embed.edit_embed(member))
+        ## under debug
+        if after.channel is not None and after.channel.id in announceChannelIds:
+            if before.channel is None:
+                # asyncio.run(embed.create_embed(member, after.channel))
+                sendEmbed = await embed.EmbedSender.create_embed(member, after.channel)
+                await botRoom.send(embed=sendEmbed)
+            else:
+                update_embed = embed.EmbedSender.edit_embed(member, after.channel, sendEmbed)
+                await sendEmbed.edit(embed=update_embed)
 
-
-# 開始処理
-@client.event
-async def on_ready() -> None:
-    await tree.sync(guild=discord.Object(id=GUILD_ID))
 
 # 起動
 client.run(BOT_TOKEN)
